eb9001aacebeaf700fde49a17d4ec83534297c88
Vladislav Belousov <vladbelousov@yahoo.com>
2024-12-05 21:36:08 +0000

    Add none-ls plugin

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,13 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  config = function()
-    local config = require('nvim-treesitter.configs')
-    config.setup({
-      ensure_installed = { 'lua', 'javascript', 'cpp', 'c', 'html', 'python' },
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			ensure_installed = { "lua", "javascript", "cpp", "html", "python" },
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
